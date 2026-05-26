e716dcd3ef10e1aa922fedcf14eadca1044da56a
sobhavesh <bhavesh.soni@coupa.com>
2026-05-26 23:03:46 +0530

    minimal 1 API  app

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 
-app = FastAPI()
-
+app = FastAPI(
+    title="Customer Management API",
+    version="1.0.0"
+)
 
 @app.get("/")
 def root():
     return {"status": "ok"}
 
-
 @app.get("/health")
 def health():
-    return {"message": "API running"}
+    return {"message": "healthy"}
