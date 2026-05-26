d72ed2faff0baa1d7ced3b1c6e762023485962f8
sobhavesh <bhavesh.soni@coupa.com>
2026-05-26 23:05:01 +0530

    minimal 2 API  app

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Customer Management API",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
